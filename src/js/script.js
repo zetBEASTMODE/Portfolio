ba5e010fb09f31f824df59827b3fddc73a81e84b
zet <zet9494@gmail.com>
2022-07-13 16:45:11 +1000

    add scrollup and work on validate form

--- src/js/script.js
+++ src/js/script.js
@@ -15,3 +15,50 @@ const counters = document.querySelectorAll('.skills__progress-item_text_subtitle
 counters.forEach( (item, i) => {
     lines[i].style.width = item.innerHTML;
 });
+
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1300) {
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
+    }
+  });
+
+  $("a[href^='#up']").click(function() {
+    var _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+  });
+
+  new WOW().init();
+
+
+  function validateForms(form) {
+    $(form).validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 2
+        },
+        email: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        name: {
+          required: "Пожалуйста, введите своё имя",
+          minlength: jQuery.validator.format("Введите минимум {0} символа")
+        },
+        email: {
+          required: "Пожалуйста, введите свою почту",
+          email: "Введите адрес своей почты example@gmail.com"
+        }
+      }
+    });
+}
+
+validateForms('#contacts-form');
+
+
